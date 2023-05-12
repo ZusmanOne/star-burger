fc2df6bffb61ae1ff7b2f3a66d681bbf33240573
nikita <bigmama93@mail.ru>
2023-05-12 14:16:00 +0300

    edit docker for production

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+
+
+echo "Django docker is fully configured successfully."
+
